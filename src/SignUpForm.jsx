ac5ddae9978363cfdda7452f28f51d21773fc50c
Konrad C <conrisc@gmail.com>
2019-12-13 09:46:27 +0100

    Adds materialize library

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -2,32 +2,39 @@ import React from 'react';
 
 export function SignUpForm(props) {
     return (
-        <div>
-            <form>
-                Email: <input type="email" />
-                <br/>
-                <label>
-                    Are you at least 18 yo?
-                    <input type="checkbox" />
-                </label>
-                <br/>
-                <label>
-                    Do you want to subscribe to our newsletter?
-                    <input type="checkbox" />
-                </label>
-                <br/>
+        <div className="row">
+            <form className="col s4 offset-s4">
+                <div className="row">
+                    <div className="input-field col s12">
+                        <input placeholder="Email" type="email" class="validate" />
+                    </div>
+                </div>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span>Are you at least 18 yo?</span>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <input type="checkbox" />
+                        <span>Subscribe to newsletter</span>
+                    </label>
+                </p>
                 <div>
-                    What's your gender?
-                    <br/>
-                    <label>
-                        <input type="radio" name="gender" value="Male" />
-                        Male
-                    </label>
-                    <br/>
-                    <label>
-                        <input type="radio" name="gender" value="Female" />
-                        Female
-                    </label>
+                    <p>What's your gender?</p>
+                    <p>
+                        <label>
+                            <input type="radio" name="gender" value="Male" />
+                            <span>Male</span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            <input type="radio" name="gender" value="Female" />
+                            <span>Female</span>
+                        </label>
+                    </p>
                 </div>
             </form>
         </div>
